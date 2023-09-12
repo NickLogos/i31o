bccd2cb02196f944a4e55bee71eb220a8afb786d
NickLogos <132320322+NickLogos@users.noreply.github.com>
2023-09-12 21:31:14 +0400

    editing label

--- i31o/i31o/ViewController.swift
+++ i31o/i31o/ViewController.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello git <3")
         view.backgroundColor = .blue
     }
-
-
 }
 
