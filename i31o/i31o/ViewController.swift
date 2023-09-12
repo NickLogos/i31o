d9cc2d608df632cdf650d1f830969bcdbc856ac2
NickLogos <132320322+NickLogos@users.noreply.github.com>
2023-09-12 21:41:32 +0400

    deletelabel

--- i31o/i31o/ViewController.swift
+++ i31o/i31o/ViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
-    
-    @IBOutlet weak var label2: UILabel!
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello git <3")
         view.backgroundColor = .blue
         label.text = "Hello"
+        
     }
 }
 
