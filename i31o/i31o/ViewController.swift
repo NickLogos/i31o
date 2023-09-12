8ead9fe1289104063f1605a1cfe345faa056b9d9
NickLogos <132320322+NickLogos@users.noreply.github.com>
2023-09-12 20:52:19 +0400

    changeColor

--- i31o/i31o/ViewController.swift
+++ i31o/i31o/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello git <3")
+        view.backgroundColor = .blue
     }
 
 
